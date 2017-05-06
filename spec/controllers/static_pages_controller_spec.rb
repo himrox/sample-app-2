0658311ca02241729fcdb81fd8928908a140e279
himrox <himrox77@gmail.com>
2017-05-06 11:34:36 +0900

    Finish static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
+  render_views
+
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+    end
+
+    it "タイトルが「Home | Ruby on Rails Tutorial Sample App」であること" do
+      get :home
+      expect(response.body).to have_title 'Home | Ruby on Rails Tutorial Sample App'
     end
   end
 
@@ -13,6 +20,35 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns http success" do
       get :help
       expect(response).to have_http_status(:success)
+    end
+
+    it "タイトルが「Help | Ruby on Rails Tutorial Sample App」であること" do
+      get :help
+      expect(response.body).to have_title 'Help | Ruby on Rails Tutorial Sample App'
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+
+    it "タイトルが「About | Ruby on Rails Tutorial Sample App」であること" do
+      get :about
+      expect(response.body).to have_title 'About | Ruby on Rails Tutorial Sample App'
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+    end
+
+    it "タイトルが「Contact | Ruby on Rails Tutorial Sample App」であること" do
+      get :contact
+      expect(response.body).to have_title 'Contact | Ruby on Rails Tutorial Sample App'
     end
   end
 
